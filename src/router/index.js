4f4c6eaeba45a1083987f4b087b62166193a1bf1
minaylosd <minaylosd@gmail.com>
2024-07-27 14:25:02 +0300

    -about page

--- src/router/index.js
+++ src/router/index.js
@@ -14,14 +14,6 @@ const router = new VueRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
       path: '/balance',
       name: 'balance',
       component: () => import('../views/BalanceHistoryView.vue')
