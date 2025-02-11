067e93acdfa52cc1aa05f829677b8d204ad5831d
minaylos-dev <minaylos.dev@gmail.com>
2025-02-11 10:29:37 +0300

    fix thumb position and track color

--- src/components/Question/QuestionScale.vue
+++ src/components/Question/QuestionScale.vue
@@ -52,42 +52,63 @@
       </li>
     </ul>
 
-    <div class="flex gap-5 mb-8">
-      <p class="mt-3 text-sm font-compact font-regular text-greytxt">
-        Очень плохо
-      </p>
+    <div class="flex mb-8 gap-7">
 
       <div>
-        <input
-          class="w-[552px] mb-6"
-          min="0"
-          max="9"
-          step="1"
-          type="range"
-          list="values"
-        />
-        <datalist class="flex justify-between w-[552px]" id="values">
-          <option
-            class="p-0 text-sm font-normal font-compact text-greytxt"
-            v-for="answer in answers"
-            :value="answer.value"
-            :key="answer.value"
-            :label="answer.value"
-          ></option>
+        <p class="mt-1 text-sm font-compact font-regular text-greytxt">
+          Очень плохо
+        </p>
+      </div>
+      
+
+      <div class="flex flex-col gap-1">
+        <div class="relative flex flex-col justify-center">
+          <div ref="indicator" class="absolute left-0 flex h-1 -translate-y-1/2 rounded-sm top-1/2 indicator bg-brand" :style="{ width: indicatorWidth }"></div>
+          <div ref="thumb" class="thumb" :style="{ left: indicatorWidth }"></div>
+          <input
+            class="w-[552px] h-8"
+            :min="answers[0].value"
+            :max="answers[answers.length-1].value"
+            v-model="rangeAnswer"
+            @input="updateRange"
+            step="1"
+            type="range"
+            list="values"
+          />
+        </div>
+
+        <datalist class="relative flex w-[552px] h-5" id="values">
+          <div
+            v-for="(answer, index) in answers"
+            :key="answer.value" :style="{ left: index * 552 / (answers.length - 1) + 'px' }"
+            class="absolute top-0 p-0 text-sm font-normal -translate-x-1/2 font-compact text-greytxt"
+          >
+            <option
+              class="p-0"
+              :value="answer.value"
+              :label="answer.value"
+            >
+              {{ answer.value }}
+            </option>
+          </div> 
         </datalist>
       </div>
 
-      <p class="mt-3 text-sm font-compact font-regular text-greytxt">
-        Очень хорошо
-      </p>
+      <div>
+        <p class="mt-1 text-sm font-compact font-regular text-greytxt">
+          Очень хорошо
+        </p>
+      </div>
+      
     </div>
 
     <div class="px-3 mb-6 w-100">
       <label
         for="commentary"
         class="block mb-1 text-sm font-normal font-compact text-greytxt"
-        >Комментарий к ответу</label
       >
+        Комментарий к ответу
+      </label>
       <textarea
         type="text"
         id="commentary"
@@ -106,14 +127,13 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 const props = defineProps({
   useEmoji: Boolean,
 });
 
 const answers = ref([
-  { value: 0, imgSrc: "/icons/0.svg" },
   { value: 1, imgSrc: "/icons/1.svg" },
   { value: 2, imgSrc: "/icons/2.svg" },
   { value: 3, imgSrc: "/icons/3.svg" },
@@ -132,39 +152,69 @@ function updateRadio(answerId) {
   selectedAnswer.value = answerId;
   isAnyChecked.value = true;
 }
+
+const thumb = ref(null);
+const indicator = ref(null);
+
+const rangeAnswer = ref(answers.value[0].value);
+
+const maxIndex = computed(() => answers.value.length - 1);
+
+const indicatorWidth = computed(() => {
+  const currentIndex = answers.value.findIndex(answer => answer.value === rangeAnswer.value);
+  if (currentIndex === -1) return '0%';
+  return `${(currentIndex / maxIndex.value) * 100}%`;
+});
+
+function updateRange(event) {
+  rangeAnswer.value = parseInt((event.target).value);
+}
 </script>
 
 <style scoped>
 input[type="range"] {
   -webkit-appearance: none;
   appearance: none;
-  height: 4px;
+  background: transparent;
+  cursor: pointer;
+  width: 100%;
+}
+
+input[type="range"]:focus {
   outline: none;
-  border-radius: 2px;
 }
 
 input[type="range"]::-webkit-slider-runnable-track {
-  height: 4px;
-  background: rgba(188, 195, 208, 0.5);
+  background-color: rgba(188, 195, 208, 0.5);
   border-radius: 2px;
+  height: 4px;
+}
+
+
+input[type="range"]::-webkit-slider-thumb {
+  opacity: 0;
 }
 
 input[type="range"]::-moz-range-track {
-  height: 4px;
-  background: rgba(188, 195, 208, 0.5);
+  background-color: rgba(188, 195, 208, 0.5);
   border-radius: 2px;
+  height: 4px;
 }
 
-input[type="range"]::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  appearance: none;
-  width: 32px;
+input[type="range"]::-moz-range-thumb {
+  opacity: 0;
+}
+
+.thumb {
+  background-color: #fff;
+  border-radius: 16px;
   height: 32px;
-  margin-top: -16px;
-  border-radius: 50%;
-  background: #fff;
+  width: 32px;
   cursor: pointer;
   box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.08),
     0px 4px 24px 0px rgba(0, 0, 0, 0.12);
+  position: absolute;
+  top: 0;
+  transform: translate(-50%, 0);
 }
 </style>
