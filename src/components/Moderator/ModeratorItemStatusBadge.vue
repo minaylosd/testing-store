b7cdf2b6fb814b655c2b6cac500f015de2acdc48
minaylos-dev <minaylos.dev@gmail.com>
2025-02-20 15:15:52 +0300

    update status

--- src/components/Moderator/ModeratorItemStatusBadge.vue
+++ src/components/Moderator/ModeratorItemStatusBadge.vue
@@ -5,14 +5,19 @@
     >Модерация пройдена</span
   >
   <span
-    v-else-if="props.status == 'inProgress'"
+    v-else-if="props.status == 'failed'"
     class="px-1 py-0.5 text-xs font-medium leading-6 text-white rounded-md font-compact bg-badgeprogress"
     >Модерация не выполнена</span
   >
   <span
-    v-else-if="props.status == 'failed'"
+    v-else-if="props.status == 'active'"
     class="px-1 py-0.5 text-xs font-medium leading-6 text-white rounded-md font-compact bg-brand"
-    >Модерация не пройдена</span
+    >Активный</span
+  >
+  <span
+    v-else-if="props.status == 'inProgress'"
+    class="px-1 py-0.5 text-xs font-medium leading-6 text-white rounded-md font-compact bg-[#45b6fc]"
+    >Отправлено на модерацию</span
   >
 </template>
 
