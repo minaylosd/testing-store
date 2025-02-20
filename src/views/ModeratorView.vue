cf06f45d2d56bc5f20e1b48de9de60bce6a8510b
minaylos-dev <minaylos.dev@gmail.com>
2025-02-20 15:33:12 +0300

    fix page layout, - unused components

--- src/views/ModeratorView.vue
+++ src/views/ModeratorView.vue
@@ -1,16 +1,11 @@
 <template>
   <main class="w-full pb-16 pt-25 bg-tertiary grow">
-    <div class="px-4 mx-auto md:px-20 max-w-none md:max-w-388 2xl:px-9">
-      <ModeratorList v-if="!isDetailsOpen" />
-      <ModeratorItemDetails v-else />
+    <div class="px-4 mx-auto max-w-none md:max-w-[1400px] md:px-9">
+      <ModeratorList />
     </div>
   </main>
 </template>
 
 <script setup>
-import { ref } from "vue";
 import ModeratorList from "@/components/Moderator/ModeratorList.vue";
-import ModeratorItemDetails from "@/components/Moderator/ModeratorItemDetails.vue";
-
-const isDetailsOpen = ref(false);
 </script>
