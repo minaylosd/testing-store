0da6c3f15c9f245cae4dcea82512f63a032ead05
minaylos-dev <minaylos.dev@gmail.com>
2025-02-20 15:32:18 +0300

    button

---
+++ src/components/ui/StartStopTestBtn.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="p-2 rounded-lg bg-tertiary">
+    <img
+      :src="props.status == 'passed' ? '/icons/stop-btn.svg' : '/icons/play-btn.svg'"
+      alt=""
+    />
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  status: String
+})
+</script>
