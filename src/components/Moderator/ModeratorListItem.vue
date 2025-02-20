4ddae51726e0523529c821f6b2c5cd22db856da3
minaylos-dev <minaylos.dev@gmail.com>
2025-02-20 15:31:52 +0300

    add link

--- src/components/Moderator/ModeratorListItem.vue
+++ src/components/Moderator/ModeratorListItem.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="grid grid-cols-[2fr_2fr_2fr_3fr_3fr] items-center py-3 border-t border-divider/50">
+  <RouterLink :to="`moderator/test/${props.item.id}`"
+    class="grid grid-cols-[2fr_2fr_2fr_3fr_3fr] items-center py-3 border-t border-divider/50">
     <div class="text-sm font-normal font-compact text-txt">
       {{ props.item.name }}
     </div>
@@ -17,17 +18,21 @@
         {{ props.item.lastUpdateDate }}
       </div>
       <div class="flex items-center gap-2">
-        <EditBtn />
+        <RouterLink :to="`moderator/edit/${props.item.id}`">
+          <EditBtn />
+        </RouterLink>
+
         <StartStopTestBtn :status="props.item.status" />
       </div>
     </div>
-  </div>
+  </RouterLink>
 </template>
 
 <script setup>
 import ModeratorItemStatusBadge from "./ModeratorItemStatusBadge.vue";
 import StartStopTestBtn from "../ui/StartStopTestBtn.vue";
 import EditBtn from "../ui/EditBtn.vue";
+import { RouterLink } from "vue-router";
 
 const props = defineProps({
   item: Object,
