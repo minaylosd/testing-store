ecb4f8fac8c09cef0d1dff59e58dc3e7cddc6c87
minaylos-dev <minaylos.dev@gmail.com>
2025-02-20 15:12:10 +0300

    render based on props

--- src/components/StatusItemBtn.vue
+++ src/components/StatusItemBtn.vue
@@ -1,13 +1,14 @@
 <template>
   <button class="p-2 rounded-lg bg-tertiary">
     <img
-      :src="isActive ? '/icons/stop-btn.svg' : '/icons/play-btn.svg'"
+      :src="props.status == 'passed' ? '/icons/stop-btn.svg' : '/icons/play-btn.svg'"
       alt=""
     />
   </button>
 </template>
 
 <script setup>
-import { ref } from "vue";
-const isActive = ref(false);
+const props = defineProps({
+  status: String
+})
 </script>
