a53403eff8c254863097c7e129de92fa45fd38a4
minaylos-dev <minaylos.dev@gmail.com>
2025-02-20 15:15:07 +0300

    add btns

--- src/components/Moderator/ModeratorListItem.vue
+++ src/components/Moderator/ModeratorListItem.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="grid grid-cols-[2fr_2fr_2fr_3fr_3fr] items-center py-3 border-t border-divider/50"
-  >
+  <div class="grid grid-cols-[2fr_2fr_2fr_3fr_3fr] items-center py-3 border-t border-divider/50">
     <div class="text-sm font-normal font-compact text-txt">
       {{ props.item.name }}
     </div>
@@ -18,8 +16,9 @@
       <div class="text-sm font-normal font-compact text-txt">
         {{ props.item.lastUpdateDate }}
       </div>
-      <div>
-        <StatusItemBtn />
+      <div class="flex items-center gap-2">
+        <EditBtn />
+        <StartStopTestBtn :status="props.item.status" />
       </div>
     </div>
   </div>
@@ -27,7 +26,9 @@
 
 <script setup>
 import ModeratorItemStatusBadge from "./ModeratorItemStatusBadge.vue";
-import StatusItemBtn from "@/components/StatusItemBtn.vue";
+import StartStopTestBtn from "../ui/StartStopTestBtn.vue";
+import EditBtn from "../ui/EditBtn.vue";
+
 const props = defineProps({
   item: Object,
 });
